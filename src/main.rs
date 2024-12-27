519461b1d06ce6fe466eb435982231eab189d3e6
Tim Janke <tim+github@janke.biz>
2024-12-27 15:41:37 +0100

    :sparkles: Add automatic database migration on startup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
 use chrono::Utc;
+use diesel::PgConnection;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 use log::{debug, error, info, trace, warn, LevelFilter};
+use std::time::Duration;
 
 mod models;
 mod schema;
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations/");
+
+pub fn run_migrations(connection: &mut PgConnection) {
+    match connection.run_pending_migrations(MIGRATIONS) {
+        Ok(ran_migrations) => {
+            if !ran_migrations.is_empty() {
+                info!(
+                    "Successfully ran {} database migrations",
+                    ran_migrations.len()
+                );
+            } else {
+                info!("No migrations had to be run since the database is up to date");
+            }
+        }
+        Err(error) => {
+            error!(
+                "Failed to run the database migrations. The error was: {}",
+                error
+            )
+        }
+    }
+}
 
 async fn setup_logging(logging_level: LevelFilter) {
     let mut base_config = fern::Dispatch::new();
@@ -29,9 +55,22 @@ async fn main() {
 
     setup_logging(LevelFilter::Trace).await;
 
-    trace!("Some trace message");
-    debug!("Some debug message");
-    info!("Some info message");
-    warn!("Some warn message");
-    error!("Some error message");
+    let database_connection_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let db_connection_pool_manager = diesel::r2d2::ConnectionManager::new(&database_connection_url);
+    let db_connection_pool = diesel::r2d2::Pool::builder()
+        .max_size(15)
+        .connection_timeout(Duration::from_secs(5))
+        .build(db_connection_pool_manager)
+        .unwrap();
+    debug!("Successfully connected to the database server");
+
+    let mut db_connection = db_connection_pool.get().unwrap_or_else(|e| {
+        error!(
+            "Could not get a database connection from the connection pool. The error was: {}",
+            e
+        );
+        std::process::exit(-1);
+    });
+    run_migrations(&mut db_connection);
+    info!("Database preparations finished");
 }
