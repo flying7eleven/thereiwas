c9fe1f4d673caaacff4c2397fc12e382dd8d9645
Tim Janke <tim+github@janke.biz>
2024-12-29 15:53:44 +0100

    :recycle: (owntracks): Prepare the code to be able to parse different request bodies

--- src/routes/owntracks.rs
+++ src/routes/owntracks.rs
@@ -3,10 +3,12 @@ use crate::models::NewLocation;
 use crate::routes::guards::RawBody;
 use crate::schema;
 use chrono::DateTime;
+use diesel::r2d2::ConnectionManager;
 use diesel::result::DatabaseErrorKind;
 use diesel::result::Error::DatabaseError;
-use diesel::RunQueryDsl;
+use diesel::{PgConnection, RunQueryDsl};
 use log::error;
+use r2d2::PooledConnection;
 use rocket::http::Status;
 use rocket::{post, State};
 use serde::{Deserialize, Serialize};
@@ -37,44 +39,51 @@ struct NewLocationRequest {
     pub created_at: Option<i64>,
 }
 
-#[post("/owntracks", data = "<raw_body>")]
-pub fn add_new_location_record(
-    db_connection_pool: &State<ThereIWasDatabaseConnection>,
-    raw_body: RawBody,
-) -> Status {
-    let new_location = match serde_json::from_slice::<NewLocationRequest>(&raw_body.0) {
-        Ok(parsed) => parsed,
-        Err(e) => {
-            let body_str = String::from_utf8_lossy(&raw_body.0);
-            error!(
-                "Received unknown or invalid JSON received (error was {}): {}",
-                e, body_str
-            );
-            return Status::UnprocessableEntity;
-        }
-    };
+#[derive(Serialize, Deserialize)]
+struct Waypoint {
+    pub rad: i64,
+    pub tst: i64,
+    pub _type: String,
+    pub rid: String,
+    pub lon: f64,
+    pub lat: f64,
+    pub desc: String,
+}
 
+#[derive(Serialize, Deserialize)]
+struct NewWaypointsRequest {
+    pub _type: String,
+    pub topic: String,
+    pub waypoints: Vec<Waypoint>,
+}
+
+fn handle_new_location_request(
+    location_request: &NewLocationRequest,
+    db_connection: &mut PooledConnection<ConnectionManager<PgConnection>>,
+) -> Status {
     let new_record = NewLocation {
-        horizontal_accuracy: new_location.acc,
-        altitude: new_location.alt,
-        latitude: new_location.lat,
-        longitude: new_location.lon,
-        report_trigger: new_location.t.clone(),
-        measurement_time: DateTime::from_timestamp(new_location.tst, 0)
+        horizontal_accuracy: location_request.acc,
+        altitude: location_request.alt,
+        latitude: location_request.lat,
+        longitude: location_request.lon,
+        report_trigger: location_request.t.clone(),
+        measurement_time: DateTime::from_timestamp(location_request.tst, 0)
             .unwrap()
             .naive_utc(),
-        vertical_accuracy: new_location.vac,
-        barometric_pressure: new_location.p,
-        topic: new_location.topic.clone().unwrap_or("unknown".to_string()),
-        created_at: new_location.created_at.map_or(None, |time_stamp| {
+        vertical_accuracy: location_request.vac,
+        barometric_pressure: location_request.p,
+        topic: location_request
+            .topic
+            .clone()
+            .unwrap_or("unknown".to_string()),
+        created_at: location_request.created_at.map_or(None, |time_stamp| {
             Some(DateTime::from_timestamp(time_stamp, 0).unwrap().naive_utc())
         }),
     };
 
-    let mut db_connection = db_connection_pool.get().unwrap();
     let query_result = diesel::insert_into(schema::locations::table)
         .values(&new_record)
-        .execute(&mut db_connection);
+        .execute(db_connection);
 
     if let Err(DatabaseError(error_kind, error_info)) = query_result {
         if let DatabaseErrorKind::UniqueViolation = error_kind {
@@ -94,4 +103,26 @@ pub fn add_new_location_record(
     }
 
     Status::NoContent
+}
+
+#[post("/owntracks", data = "<raw_body>")]
+pub fn add_new_location_record(
+    db_connection_pool: &State<ThereIWasDatabaseConnection>,
+    raw_body: RawBody,
+) -> Status {
+    let new_location = match serde_json::from_slice::<NewLocationRequest>(&raw_body.0) {
+        Ok(parsed) => parsed,
+        Err(e) => {
+            let body_str = String::from_utf8_lossy(&raw_body.0);
+            error!(
+                "Received unknown or invalid JSON received (error was {}): {}",
+                e, body_str
+            );
+            return Status::UnprocessableEntity;
+        }
+    };
+
+    let mut db_connection = db_connection_pool.get().unwrap();
+
+    handle_new_location_request(&new_location, &mut db_connection)
 }
